c93feb231bdf8dc21be69360d6015d52c9ebc683
Hamza Amin Khan <khansubscriptions79@gmail.com>
2026-08-25 02:28:23 +0500

    created new widget

---
+++ lib/screens/moments/screens/moment_detail_screen.dart
@@ -0,0 +1,338 @@
+import 'package:flutter/material.dart';
+import 'package:junaya_voicechat_app/screens/moments/data/moment_model.dart';
+import 'package:junaya_voicechat_app/screens/moments/widgets/moment_media.dart';
+
+import '../widgets/moment_header.dart';
+
+import '../widgets/reaction_bar.dart';
+import '../widgets/comments_bottom_sheet.dart';
+
+
+
+class MomentDetailScreen extends StatelessWidget {
+
+  final Moment moment;
+
+
+  const MomentDetailScreen({
+
+    super.key,
+
+    required this.moment,
+
+  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+
+      backgroundColor:
+      const Color(0xff090909),
+
+
+      appBar:
+      AppBar(
+
+        backgroundColor:
+        Colors.transparent,
+
+        elevation:
+        0,
+
+        iconTheme:
+        const IconThemeData(
+          color: Colors.white,
+        ),
+
+        title:
+        const Text(
+          "Moment",
+          style:
+          TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+      ),
+
+
+
+      body:
+      ListView(
+
+        padding:
+        const EdgeInsets.only(
+          bottom:40,
+        ),
+
+
+        children: [
+
+
+
+          MomentHeader(
+
+            moment:
+            moment,
+
+
+            onDelete:
+                (){
+
+              Navigator.pop(
+                context,
+              );
+
+            },
+
+          ),
+
+
+
+
+
+          const SizedBox(
+            height:16,
+          ),
+
+
+
+
+
+
+          if(moment.media.isNotEmpty)
+
+            SizedBox(
+
+              height:300,
+
+
+              child:
+              ListView.builder(
+
+                scrollDirection:
+                Axis.horizontal,
+
+
+                padding:
+                const EdgeInsets.symmetric(
+                  horizontal:16,
+                ),
+
+
+                itemCount:
+                moment.media.length,
+
+
+                itemBuilder:
+                    (context,index){
+
+
+                  final media =
+                  moment.media[index];
+
+
+
+                  return Padding(
+
+                    padding:
+                    const EdgeInsets.only(
+                      right:12,
+                    ),
+
+
+                    child:
+                    MomentMediaWidget(
+                      media: [
+                        media,
+                      ],
+                    )
+
+                  );
+
+
+                },
+
+              ),
+
+            ),
+
+
+
+
+
+          const SizedBox(
+            height:20,
+          ),
+
+
+
+
+
+
+          Padding(
+
+            padding:
+            const EdgeInsets.symmetric(
+              horizontal:16,
+            ),
+
+
+            child:
+            Text(
+
+              moment.caption,
+
+
+              style:
+              const TextStyle(
+
+                color:
+                Colors.white,
+
+                fontSize:
+                18,
+
+              ),
+
+            ),
+
+          ),
+
+
+
+
+
+
+          const SizedBox(
+            height:20,
+          ),
+
+
+
+
+
+
+          Padding(
+
+            padding:
+            const EdgeInsets.symmetric(
+              horizontal:16,
+            ),
+
+
+            child:
+            ReactionBar(
+
+              moment:
+              moment,
+
+            ),
+
+          ),
+
+
+
+
+
+
+          const SizedBox(
+            height:20,
+          ),
+
+
+
+
+
+
+          Padding(
+
+            padding:
+            const EdgeInsets.symmetric(
+              horizontal:16,
+            ),
+
+
+            child:
+            ElevatedButton.icon(
+
+              style:
+              ElevatedButton.styleFrom(
+
+                backgroundColor:
+                const Color(0xff1c1c1c),
+
+              ),
+
+
+              icon:
+              const Icon(
+                Icons.comment,
+                color:Colors.white,
+              ),
+
+
+              label:
+              Text(
+
+                "${moment.commentsCount} Comments",
+
+                style:
+                const TextStyle(
+                  color:Colors.white,
+                ),
+
+              ),
+
+
+
+              onPressed:
+                  (){
+
+
+                showModalBottomSheet(
+
+                  context:
+                  context,
+
+
+                  backgroundColor:
+                  Colors.transparent,
+
+
+                  isScrollControlled:
+                  true,
+
+
+                  builder:
+                      (_) =>
+                      CommentsBottomSheet(
+
+                        momentId:
+                        moment.id,
+
+                      ),
+
+                );
+
+
+              },
+
+            ),
+
+          ),
+
+        ],
+
+      ),
+
+    );
+
+  }
+
+}
---
+++ lib/screens/moments/widgets/fullscreen_media_viewer.dart
@@ -0,0 +1,334 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
+import 'package:junaya_voicechat_app/screens/moments/data/moment_model.dart';
+
+
+
+class FullscreenMediaViewer extends StatefulWidget {
+
+  final List<MomentMedia> media;
+
+  final int initialIndex;
+
+
+  const FullscreenMediaViewer({
+
+    super.key,
+
+    required this.media,
+
+    this.initialIndex = 0,
+
+  });
+
+
+
+  @override
+  State<FullscreenMediaViewer> createState() =>
+      _FullscreenMediaViewerState();
+
+}
+
+
+
+
+
+class _FullscreenMediaViewerState
+    extends State<FullscreenMediaViewer> {
+
+
+  late PageController _pageController;
+
+
+  late int currentIndex;
+
+
+
+  @override
+  void initState() {
+
+    super.initState();
+
+
+    currentIndex =
+        widget.initialIndex;
+
+
+    _pageController =
+        PageController(
+          initialPage:
+          currentIndex,
+        );
+
+  }
+
+
+
+
+
+  @override
+  void dispose() {
+
+    _pageController.dispose();
+
+    super.dispose();
+
+  }
+
+
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+
+      backgroundColor:
+      Colors.black,
+
+
+      body:
+      Stack(
+
+        children: [
+
+
+
+          PhotoViewGallery.builder(
+
+            pageController:
+            _pageController,
+
+
+            itemCount:
+            widget.media.length,
+
+
+            onPageChanged:
+                (index) {
+
+              setState(() {
+
+                currentIndex =
+                    index;
+
+              });
+
+            },
+
+
+            builder:
+                (context,index) {
+
+
+              final item =
+              widget.media[index];
+
+
+
+              return PhotoViewGalleryPageOptions(
+
+                imageProvider:
+                _imageProvider(
+                  item.url,
+                ),
+
+
+                minScale:
+                PhotoViewComputedScale.contained,
+
+
+                maxScale:
+                PhotoViewComputedScale.covered *
+                    3,
+
+
+                heroAttributes:
+                PhotoViewHeroAttributes(
+
+                  tag:
+                  item.id,
+
+                ),
+
+              );
+
+
+            },
+
+
+            backgroundDecoration:
+            const BoxDecoration(
+
+              color:
+              Colors.black,
+
+            ),
+
+          ),
+
+
+
+
+
+          Positioned(
+
+            top:
+            MediaQuery.of(context)
+                .padding
+                .top +
+                10,
+
+
+            left:
+            15,
+
+
+            child:
+            IconButton(
+
+              onPressed:
+                  () =>
+                  Navigator.pop(
+                    context,
+                  ),
+
+
+              icon:
+              const Icon(
+
+                Icons.close,
+
+                color:
+                Colors.white,
+
+                size:
+                32,
+
+              ),
+
+            ),
+
+          ),
+
+
+
+
+
+
+          if(widget.media.length > 1)
+
+            Positioned(
+
+              bottom:
+              30,
+
+
+              left:
+              0,
+
+
+              right:
+              0,
+
+
+              child:
+              Center(
+
+                child:
+                Container(
+
+                  padding:
+                  const EdgeInsets.symmetric(
+                    horizontal:12,
+                    vertical:6,
+                  ),
+
+
+                  decoration:
+                  BoxDecoration(
+
+                    color:
+                    Colors.black54,
+
+
+                    borderRadius:
+                    BorderRadius.circular(
+                      20,
+                    ),
+
+                  ),
+
+
+                  child:
+                  Text(
+
+                    "${currentIndex + 1}/${widget.media.length}",
+
+
+                    style:
+                    const TextStyle(
+
+                      color:
+                      Colors.white,
+
+                    ),
+
+                  ),
+
+                ),
+
+              ),
+
+            ),
+
+
+        ],
+
+      ),
+
+    );
+
+  }
+
+
+
+
+
+
+
+  ImageProvider _imageProvider(
+      String path,
+      ) {
+
+
+    if(path.startsWith(
+      "http",
+    )) {
+
+      return NetworkImage(
+        path,
+      );
+
+    }
+
+
+
+    return FileImage(
+      File(
+        path,
+      ),
+    );
+
+  }
+
+
+}
