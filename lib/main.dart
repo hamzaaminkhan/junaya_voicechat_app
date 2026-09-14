65b7113f16ad48c9a91745d9c04273b11ff732ba
Hamza Amin Khan <khansubscriptions79@gmail.com>
2026-09-14 22:12:58 +0500

    modified code

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ Future<void> main() async {
   // BACKEND API INITIALIZATION
   // ==========================================
 
+
   ApiClient.instance.initialize();
 
   // ==========================================
